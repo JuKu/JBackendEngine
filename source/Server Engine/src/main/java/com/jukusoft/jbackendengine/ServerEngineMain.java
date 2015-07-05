4d29e5058e256d2760f2f2ebdfd3a6b416dc0d76
JuKu <kuenzel.justin@t-online.de>
2015-07-05 22:55:57 +0200

    change ServerEngineMain.

--- source/Server Engine/src/main/java/com/jukusoft/jbackendengine/ServerEngineMain.java
+++ source/Server Engine/src/main/java/com/jukusoft/jbackendengine/ServerEngineMain.java
@@ -1,16 +1,6 @@
 package com.jukusoft.jbackendengine;
 
-import com.corundumstudio.socketio.AckRequest;
-import com.corundumstudio.socketio.SocketIOClient;
-import com.corundumstudio.socketio.listener.ConnectListener;
-import com.corundumstudio.socketio.listener.DataListener;
-import com.corundumstudio.socketio.listener.DisconnectListener;
 import com.jukusoft.jbackendengine.impl.DefaultBackendEngine;
-import com.jukusoft.jbackendengine.module.IModuleManager;
-import com.jukusoft.jbackendengine.module.exception.ModuleNotFoundException;
-import com.jukusoft.jbackendengine.servermodule.socketio.SocketIOModule;
-import com.jukusoft.jbackendengine.servermodule.socketio.socketio.ISocketIOService;
-import com.jukusoft.jbackendengine.service.exception.IServiceNotFoundException;
 
 import java.io.File;
 import java.io.IOException;
