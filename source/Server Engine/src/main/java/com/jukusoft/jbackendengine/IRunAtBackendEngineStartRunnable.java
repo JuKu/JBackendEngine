4d73be22be6283a0216a8c1a7f4431955f2dece4
JuKu <kuenzel.justin@t-online.de>
2015-07-06 20:29:07 +0200

    added interface IRunAtBackendEngineStartRunnable.

---
+++ source/Server Engine/src/main/java/com/jukusoft/jbackendengine/IRunAtBackendEngineStartRunnable.java
@@ -0,0 +1,10 @@
+package com.jukusoft.jbackendengine;
+
+/**
+ * Created by Justin on 06.07.2015.
+ */
+public interface IRunAtBackendEngineStartRunnable {
+
+    public void run (IEditableBackendEngine backendEngine);
+
+}
