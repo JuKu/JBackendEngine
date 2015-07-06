e10b9d6452e034c37bb6c451a6d53eb565439304
JuKu <kuenzel.justin@t-online.de>
2015-07-06 20:30:43 +0200

    change DefaultBackendEngine.

--- source/Server Engine/src/main/java/com/jukusoft/jbackendengine/impl/DefaultBackendEngine.java
+++ source/Server Engine/src/main/java/com/jukusoft/jbackendengine/impl/DefaultBackendEngine.java
@@ -2,6 +2,7 @@ package com.jukusoft.jbackendengine.impl;
 
 import com.jukusoft.jbackendengine.AbstractBackendEngine;
 import com.jukusoft.jbackendengine.IEditableBackendEngine;
+import com.jukusoft.jbackendengine.IRunAtBackendEngineStartRunnable;
 
 import java.io.File;
 
@@ -9,6 +10,15 @@ import java.io.File;
  * Created by Justin on 02.07.2015.
  */
 public class DefaultBackendEngine extends AbstractBackendEngine {
+
+    public DefaultBackendEngine (IRunAtBackendEngineStartRunnable runAtBackendEngineStartRunnable) {
+        super(new File("./config/"));
+        runAtBackendEngineStartRunnable.run(this);
+    }
+
+    public DefaultBackendEngine (File configDir) {
+        super(configDir);
+    }
 
     public DefaultBackendEngine () {
         super(new File("./config/"));
