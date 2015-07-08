900f3698278893ad3ccaef461dfbc260db5c7c58
JuKu <kuenzel.justin@t-online.de>
2015-07-08 16:34:04 +0200

    added ExampleMain.

---
+++ examples/JBackendEngine LocalServerSettings Example/JBackendEngine LocalServerSettings Module/src/main/java/com/jukusoft/example/localserversettings/ExampleMain.java
@@ -0,0 +1,12 @@
+package com.jukusoft.example.localserversettings;
+
+/**
+ * Created by Justin on 08.07.2015.
+ */
+public class ExampleMain {
+
+    public static void main (String[] args) {
+        //
+    }
+
+}
