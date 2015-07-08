067fdadcefb5f2e972268b60134127675e66b457
JuKu <kuenzel.justin@t-online.de>
2015-07-08 16:08:15 +0200

    added asynctask example.

---
+++ examples/asynctask/JBackendEngine AsyncTask Example/src/main/java/com/jukusoft/jbackendengine/example/asynctask/ExampleMain.java
@@ -0,0 +1,12 @@
+package com.jukusoft.jbackendengine.example.asynctask;
+
+/**
+ * Created by Justin on 08.07.2015.
+ */
+public class ExampleMain {
+
+    public static void main (String[] args) {
+        //
+    }
+
+}
