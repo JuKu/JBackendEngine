ed439dfaa5a361b92d520dbbf8cef797fb3c8a31
JuKu <kuenzel.justin@t-online.de>
2015-07-10 20:25:01 +0200

    added PostgreSQL module.

---
+++ source/PostgreSQL Module/src/main/java/com/jukusoft/jbackendengine/serverbackendengine/servermodule/postgresql/PostgreSQLModule.java
@@ -0,0 +1,35 @@
+package com.jukusoft.jbackendengine.serverbackendengine.servermodule.postgresql;
+
+import com.jukusoft.jbackendengine.backendengine.IBackendEngine;
+import com.jukusoft.jbackendengine.backendengine.module.ModuleInfo;
+import com.jukusoft.jbackendengine.backendengine.module.impl.Module;
+
+/**
+ * Created by Justin on 10.07.2015.
+ */
+@ModuleInfo(
+        moduleName = "postgresql-connector",
+        version = "1.0.0",
+        description = "A database connector fpr postgresql",
+        author = "Service4All",
+        priority = ModuleInfo.Priority.DB,
+        dependencies = {}
+)
+public class PostgreSQLModule extends Module {
+
+    @Override
+    public void onInitialize(IBackendEngine backendEngine) {
+        //
+    }
+
+    @Override
+    public void start(IBackendEngine backendEngine) {
+        //
+    }
+
+    @Override
+    public void stop() {
+        //
+    }
+
+}
