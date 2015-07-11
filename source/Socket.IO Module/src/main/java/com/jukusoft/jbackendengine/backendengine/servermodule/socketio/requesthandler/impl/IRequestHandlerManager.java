5b057495ae07d567ba8e88e81c807f2d8f757e5e
JuKu <kuenzel.justin@t-online.de>
2015-07-11 18:58:22 +0200

    added interface IRequestHandlerManager.

---
+++ source/Socket.IO Module/src/main/java/com/jukusoft/jbackendengine/backendengine/servermodule/socketio/requesthandler/impl/IRequestHandlerManager.java
@@ -0,0 +1,10 @@
+package com.jukusoft.jbackendengine.backendengine.servermodule.socketio.requesthandler.impl;
+
+/**
+ * Created by Justin on 11.07.2015.
+ */
+public interface IRequestHandlerManager {
+
+    public void registerRequestHandler (String eventname);
+
+}
