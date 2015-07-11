f5a02109eea88539a118a05b5a0dc4a6adc841e6
JuKu <kuenzel.justin@t-online.de>
2015-07-11 19:01:03 +0200

    change IRequestHandlerManager.

--- source/Socket.IO Module/src/main/java/com/jukusoft/jbackendengine/backendengine/servermodule/socketio/requesthandler/impl/IRequestHandlerManager.java
+++ source/Socket.IO Module/src/main/java/com/jukusoft/jbackendengine/backendengine/servermodule/socketio/requesthandler/impl/IRequestHandlerManager.java
@@ -1,10 +1,17 @@
 package com.jukusoft.jbackendengine.backendengine.servermodule.socketio.requesthandler.impl;
 
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.jukusoft.jbackendengine.backendengine.IBackendEngine;
+import com.jukusoft.jbackendengine.backendengine.servermodule.socketio.requesthandler.IRequestHandler;
+
 /**
  * Created by Justin on 11.07.2015.
  */
 public interface IRequestHandlerManager {
 
-    public void registerRequestHandler (String eventname);
+    public void registerRequestHandler (String eventname, IRequestHandler requestHandler);
+    public void removeRequestHandler (String eventname, IRequestHandler requestHandler);
+    public void request (String eventname, SocketIOClient client, String data, IBackendEngine backendEngine, AckRequest ackRequest);
 
 }
